0a27e8a5f6c388bb30177e739ec05c79748c97b9
GitNaf <114709801+NafMn@users.noreply.github.com>
2024-07-28 19:17:54 +0700

    update : footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,28 @@
 // darkmode and lightmode toggle
 const themeToggleBtn = document.getElementById("theme-toggle");
 const htmlElement = document.documentElement;
+const logoTheme = document.getElementById("logo-theme");
+const animeIcon = document.getElementById("anime-icon");
+const textMode = document.getElementById("text-mode");
+const logoMode = document.getElementById("logo-mode");
 
 themeToggleBtn.addEventListener("click", () => {
   if (htmlElement.classList.contains("dark")) {
     htmlElement.classList.remove("dark");
     localStorage.setItem("theme", "light");
+    logoTheme.src = "assets/images/logo-black.png";
+    logoMode.src = "assets/images/logo-black.png";
+    animeIcon.src = "assets/images/hitori-black.png";
+    textMode.innerHTML = "Light Mode";
+
   } else {
     htmlElement.classList.add("dark");
     localStorage.setItem("theme", "dark");
+    logoTheme.src = "assets/images/logo.png";
+    logoMode.src = "assets/images/logo.png";
+    animeIcon.src = "assets/images/hitori.webp";
+    textMode.innerHTML = "Dark Mode";
+    
   }
 });
 
@@ -23,3 +37,6 @@ if (savedTheme) {
     htmlElement.classList.add("dark");
   }
 }
+
+
+
